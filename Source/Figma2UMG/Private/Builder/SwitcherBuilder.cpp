269cd204a561856d14cdb35a5e070a9d94253799
BetoBadeira <64774010+BetoBadeira@users.noreply.github.com>
2024-03-02 11:26:47 +0100

    Adding the SwitcherWidget as part of the AddVariable as it's used as a variable for the function during the patch

--- Source/Figma2UMG/Private/Builder/SwitcherBuilder.cpp
+++ Source/Figma2UMG/Private/Builder/SwitcherBuilder.cpp
@@ -4,19 +4,12 @@
 #include "Builder/SwitcherBuilder.h"
 
 #include "WidgetBlueprintBuilder.h"
+#include "Blueprint/WidgetTree.h"
 #include "Components/WidgetSwitcher.h"
 
 TObjectPtr<UWidgetSwitcher> FSwitcherBuilder::Patch(TObjectPtr<UWidget> WidgetToPatch, UObject* AssetOuter)
 {
 	WidgetSwitcher = WidgetToPatch ? Cast<UWidgetSwitcher>(WidgetToPatch) : nullptr;
-	if (WidgetSwitcher == nullptr)
-	{
-		WidgetToPatch = WidgetSwitcher = NewObject<UWidgetSwitcher>(AssetOuter, *PropertyName);
-	}
-	else if (WidgetSwitcher->GetName() != PropertyName)
-	{
-		WidgetSwitcher->Rename(*PropertyName);
-	}
 
 	WidgetBlueprintBuilder::PatchSwitchFunction(Cast<UWidgetBlueprint>(AssetOuter->GetOuter()), WidgetSwitcher, PropertyName, PropertyDefinition.VariantOptions);
 
@@ -25,6 +18,30 @@ TObjectPtr<UWidgetSwitcher> FSwitcherBuilder::Patch(TObjectPtr<UWidget> WidgetTo
 
 void FSwitcherBuilder::AddVariation(UWidgetBlueprint* WidgetBP)
 {
+	// The UWidgetSwitcher needs to be created during the creation of the UAsset so it can be compiled and visible during patch.
+	TArray<UWidget*> Widgets;
+	WidgetBP->WidgetTree->GetAllWidgets(Widgets);
+	FString NameToFind = PropertyName;
+	UWidget** Found = Widgets.FindByPredicate([NameToFind](const UWidget* Widget) {return Widget->IsA<UWidgetSwitcher>() && Widget->GetName() == NameToFind; });
+	WidgetSwitcher = Found ? Cast<UWidgetSwitcher>(*Found) : nullptr;
+	if (WidgetSwitcher == nullptr)
+	{
+		WidgetSwitcher = NewObject<UWidgetSwitcher>(WidgetBP->WidgetTree, *PropertyName);
+		if (Widgets.IsEmpty())
+		{
+			WidgetBP->WidgetTree->RootWidget = WidgetSwitcher;
+		}
+		else
+		{
+			UWidgetSwitcher* ParentSwitch = Cast<UWidgetSwitcher>(Widgets.Last());
+			ParentSwitch->AddChild(WidgetSwitcher);
+		}
+	}
+	else if (WidgetSwitcher->GetName() != PropertyName)
+	{
+		WidgetSwitcher->Rename(*PropertyName);
+	}
+
 	WidgetBlueprintBuilder::CreateSwitchFunction(WidgetBP, PropertyName, PropertyDefinition.VariantOptions);
 }
 
