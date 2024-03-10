925dd5c409fb9a7e9c374ba2849cf574646c305c
BetoBadeira <64774010+BetoBadeira@users.noreply.github.com>
2024-03-10 09:27:28 +0100

    Adding check before the hack

--- Source/Figma2UMG/Private/REST/FigmaRequest.cpp
+++ Source/Figma2UMG/Private/REST/FigmaRequest.cpp
@@ -20,26 +20,30 @@ void IFigmaRequest::StartDownload()
 	HttpRequest->SetHeader(FIGMA_ACCESSTOLKENS_HEADER, AccessToken);
 	HttpRequest->SetHeader(FString("Host"), FIGMA_HOST);
 	HttpRequest->ProcessRequest();
+
+
 	// This section bellow is a hack due to the FCurlHttpRequest::SetupRequest() always adding the header Content-Length. Adding it makes the Figma AIP return the error 400 
 	// To avoid reimplementing the curl class, we need to maually remove the Header item.
 	// This will need update and check if it has any change in the FCurlHttpRequest size so the memory offset of Header changed.
 
-	int HeaderAddressOffset = 0;
+	if (!HttpRequest->GetHeader(TEXT("Content-Length")).IsEmpty())
+	{
+		int HeaderAddressOffset = 0;
 
 #if WITH_CURL
 #if (ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION == 3 && ENGINE_PATCH_VERSION == 2)
-	HeaderAddressOffset = 256;
+		HeaderAddressOffset = 256;
 #endif
 #endif
 
-	if (HeaderAddressOffset > 0)
-	{
-		IHttpRequest* HttpRequestPtr = &(HttpRequest.Get());
-		void* HeaderAddress = reinterpret_cast<void*>(reinterpret_cast<int64>(HttpRequestPtr) + HeaderAddressOffset);
-		TMap<FString, FString>* HeadersPtr = static_cast<TMap<FString, FString>*>(HeaderAddress);
-		HeadersPtr->Remove(TEXT("Content-Length"));
+		if (HeaderAddressOffset > 0)
+		{
+			IHttpRequest* HttpRequestPtr = &(HttpRequest.Get());
+			void* HeaderAddress = reinterpret_cast<void*>(reinterpret_cast<int64>(HttpRequestPtr) + HeaderAddressOffset);
+			TMap<FString, FString>* HeadersPtr = static_cast<TMap<FString, FString>*>(HeaderAddress);
+			HeadersPtr->Remove(TEXT("Content-Length"));
+		}
 	}
-
 	// End of Hack
 }
 
