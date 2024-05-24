efd5144a34a67da8ee43101e66097809ced2eed9
BetoBadeira <nkbeto80@gmail.com>
2024-05-24 21:18:44 +0200

    batch to delete privates

---
+++ DeletePrivates.bat
@@ -0,0 +1,3 @@
+rmdir "D:\Projects\Figma2UMG-Release\5.2\Figma2UMG\Source\Figma2UMG\Private" /s /q
+rmdir "D:\Projects\Figma2UMG-Release\5.3\Figma2UMG\Source\Figma2UMG\Private" /s /q
+rmdir "D:\Projects\Figma2UMG-Release\5.4\Figma2UMG\Source\Figma2UMG\Private" /s /q
