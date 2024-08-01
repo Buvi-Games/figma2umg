35b6794d4fdde793ed9661e5f4c159b7bf55ecff
BetoBadeira <nkbeto80@gmail.com>
2024-08-02 00:13:25 +0200

    Giving Access to the Node on the AssetBuilder

--- Source/Figma2UMG/Private/Builder/Asset/AssetBuilder.cpp
+++ Source/Figma2UMG/Private/Builder/Asset/AssetBuilder.cpp
@@ -9,6 +9,11 @@ void IAssetBuilder::SetNode(const FString& InFileKey, const UFigmaNode* InNode)
 	Node = InNode;
 }
 
+const UFigmaNode* IAssetBuilder::GetNode() const
+{
+	return Node;
+}
+
 void IAssetBuilder::AddPackages(TArray<UPackage*>& Packages) const
 {
 	if (UPackage* Package = GetAssetPackage())
